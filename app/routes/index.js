05f0511d58a5aee5530a13942637059cc7fe1b87
roelver <roel@romaniflo.nl>
2016-03-03 23:17:09 +0100

    working solution

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,27 @@
 'use strict';
 
-var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
+var multer = require('multer');
+var fs     = require('fs');
+var upload = multer({ dest: 'uploads/' }).single('fupload');
 
-module.exports = function (app, db) {
-   var clickHandler = new ClickHandler(db);
+module.exports = function (app) {
 
+  app.post('/upload', upload, function (req, res) {
+  	  console.log(req.file);
+  	  var msg = {
+         name: req.file.originalname,
+         size: req.file.size
+       };
+     // Delete the uploaded file, because we know what we want to know
+     fs.unlink(req.file.path);
+
+     res.json(msg);
+  });
+
+ 
    app.route('/')
       .get(function (req, res) {
          res.sendFile(process.cwd() + '/public/index.html');
       });
 
-   app.route('/api/clicks')
-      .get(clickHandler.getClicks)
-      .post(clickHandler.addClick)
-      .delete(clickHandler.resetClicks);
 };
--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 'use strict';
 
 var express = require('express');
-var mongo = require('mongodb');
 var routes = require('./app/routes/index.js');
 
 var app = express();
 
-mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
-
-   if (err) {
-      throw new Error('Database failed to connect!');
-   } else {
-      console.log('Successfully connected to MongoDB on port 27017.');
-   }
-
    app.use('/public', express.static(process.cwd() + '/public'));
    app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-   routes(app, db);
+   routes(app);
 
-   app.listen(3000, function () {
-      console.log('Node.js listening on port 3000...');
+   var port = process.env.PORT || 3000;
+
+   app.listen(port, function () {
+      console.log('Node.js listening on port '+port+'...');
    });
-
-});
